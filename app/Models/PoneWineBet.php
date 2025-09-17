0ff7c4318016a54184bd98d213b3344c32e744b8
AMK1215 <aungmyokhaing1215@gmail.com>
2025-09-17 10:15:44 +0630

    client balance updated by amk

--- app/Models/PoneWineBet.php
+++ app/Models/PoneWineBet.php
@@ -10,4 +10,20 @@ class PoneWineBet extends Model
     use HasFactory;
 
     protected $fillable = ['room_id', 'match_id', 'status', 'win_number'];
+
+    /**
+     * Store game match data for Pone Wine
+     * 
+     * @param array $gameMatchData
+     * @return PoneWineBet
+     */
+    public static function storeGameMatchData(array $gameMatchData): self
+    {
+        return self::create([
+            'room_id' => $gameMatchData['roomId'],
+            'match_id' => $gameMatchData['matchId'],
+            'win_number' => $gameMatchData['winNumber'],
+            'status' => 1, // Default status (1 = active, 0 = inactive)
+        ]);
+    }
 }
