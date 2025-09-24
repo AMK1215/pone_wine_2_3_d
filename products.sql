605f21e2e7be48309c46aaf300d2b500a320d0e0
AMK1215 <aungmyokhaing1215@gmail.com>
2025-09-24 10:59:58 +0630

    pushbet api updated

---
+++ products.sql
@@ -0,0 +1,29 @@
+INSERT INTO "public"."products" ("id", "provider", "currency", "status", "provider_id", "provider_product_id", "product_code", "product_name", "game_type", "product_title", "short_name", "order", "game_list_status", "created_at", "updated_at") VALUES
+(1, 'Yee Bet', 'MMK', 'ACTIVATED', 3, 1146, '1016', 'yee_bet', 'LIVE_CASINO', 'YeeBet', NULL, 1, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41');
+INSERT INTO "public"."products" ("id", "provider", "currency", "status", "provider_id", "provider_product_id", "product_code", "product_name", "game_type", "product_title", "short_name", "order", "game_list_status", "created_at", "updated_at") VALUES
+(2, 'SA Gaming', 'MMK', 'ACTIVATED', 8, 1279, '1185', 'sa_gaming', 'LIVE_CASINO', 'SA Gaming', NULL, 2, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41');
+INSERT INTO "public"."products" ("id", "provider", "currency", "status", "provider_id", "provider_product_id", "product_code", "product_name", "game_type", "product_title", "short_name", "order", "game_list_status", "created_at", "updated_at") VALUES
+(3, 'SpadeGaming', 'MMK2', 'ACTIVATED', 10, 1327, '1221', 'spadegaming', 'SLOT', 'SpadeGaming', NULL, 3, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41');
+INSERT INTO "public"."products" ("id", "provider", "currency", "status", "provider_id", "provider_product_id", "product_code", "product_name", "game_type", "product_title", "short_name", "order", "game_list_status", "created_at", "updated_at") VALUES
+(4, 'Live22', 'MMK', 'ACTIVATED', 11, 1237, '1018', 'live_22', 'SLOT', 'Live22', NULL, 4, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(5, 'WMCasino', 'MMK', 'ACTIVATED', 17, 1142, '1020', 'wm_casino', 'LIVE_CASINO', 'WMCasino', NULL, 5, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(6, 'PG Soft', 'MMK2', 'ACTIVATED', 31, 1141, '1007', 'pg_soft', 'SLOT', 'PGSoft', NULL, 6, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(7, 'PragmaticPlay', 'MMK', 'ACTIVATED', 32, 1184, '1006', 'pragmatic_play', 'LIVE_CASINO', 'PragmaticPlay', NULL, 7, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(8, 'PragmaticPlay', 'MMK', 'ACTIVATED', 32, 1185, '1006', 'pragmatic_play', 'SLOT', 'PragmaticPlay', NULL, 8, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(9, 'PragmaticPlay', 'MMK', 'ACTIVATED', 32, 1300, '1006', 'pragmatic_play', 'LIVE_CASINO_PREMIUM', 'PragmaticPlay', NULL, 9, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(10, 'Dream Gaming', 'MMK', 'ACTIVATED', 33, 1188, '1052', 'dream_gaming', 'LIVE_CASINO', 'Dream Gaming', NULL, 10, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(11, 'AdvantPlay', 'MMK', 'ACTIVATED', 39, 1326, '1204', 'advantplay', 'SLOT', 'Advantplay', NULL, 11, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(12, 'JDB', 'MMK', 'ACTIVATED', 41, 1194, '1085', 'jdb', 'SLOT', 'JDB', NULL, 12, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(13, 'PlayStar', 'MMK', 'ACTIVATED', 42, 1174, '1050', 'playstar', 'SLOT', 'PlayStar', NULL, 13, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(14, 'CQ9', 'MMK', 'ACTIVATED', 47, 1186, '1009', 'cq9', 'SLOT', 'CQ9', NULL, 14, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(15, 'Jili', 'MMK', 'ACTIVATED', 48, 1144, '1091', 'jili_tcg', 'SLOT', 'JiLi', NULL, 15, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(16, 'PlayAce', 'MMK3', 'ACTIVATED', 50, 1312, '1203', 'playace', 'LIVE_CASINO', 'PlayAce', NULL, 16, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(17, 'BoomingGames', 'MMK', 'ACTIVATED', 68, 1280, '1115', 'booming_games', 'SLOT', 'Booming Games', NULL, 17, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(18, 'WOW GAMING', 'MMK', 'ACTIVATED', 101, 1177, '1148', 'wow_gaming', 'SLOT', 'WOW Gaming', NULL, 18, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(19, 'AI Live Casino', 'MMK', 'ACTIVATED', 102, 1178, '1149', 'ai_live_casino', 'LIVE_CASINO', 'AI Live Casino', NULL, 19, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(20, 'HACKSAW', 'MMK', 'ACTIVATED', 105, 1230, '1153', 'hacksaw', 'SLOT', 'Hacksaw', NULL, 20, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(21, 'BIGPOT', 'MMK', 'ACTIVATED', 106, 1235, '1154', 'bigpot', 'SLOT', 'BIGPOT', NULL, 21, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(22, 'EPICWIN', 'MMK', 'ACTIVATED', 112, 1240, '1160', 'epicwin', 'SLOT', 'EpicWin', NULL, 22, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(23, 'Fachai', 'MMK', 'ACTIVATED', 113, 1245, '1079', 'fachai', 'SLOT', 'FaChai', NULL, 23, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(24, 'N2', 'MMK', 'ACTIVATED', 115, 1248, '1163', 'novomatic', 'SLOT', 'NOVOMatic', NULL, 24, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41'),
+(25, 'SmartSoft', 'MMK', 'MAINTAINED', 118, 1256, '1170', 'smartsoft', 'SLOT', 'SmartSoft', NULL, 25, 't', '2025-09-24 03:43:41', '2025-09-24 03:43:41');
